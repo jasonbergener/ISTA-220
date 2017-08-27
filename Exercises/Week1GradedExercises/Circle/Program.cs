670f69be68fa583c34ad825f44c61eb57a04f96c
jasonbergener <30800131+jasonbergener@users.noreply.github.com>
2017-08-27 09:55:23 -0400

    Refined code to include input validation.

--- Exercises/Week1GradedExercises/Circle/Program.cs
+++ Exercises/Week1GradedExercises/Circle/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Circle
 {
@@ -15,8 +11,16 @@ namespace Circle
             {
                 Console.WriteLine("This application provides the circumference and area of a circle.");
                 Console.Write("Enter the radius of the circle: ");
-                value = double.Parse(Console.ReadLine());
-            } while (value < 0);
+                try
+                {
+                    value = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please use only numbers.");
+                    value = -1;
+                }
+            } while (value <= 0);
 
             Console.WriteLine($"The circumference of the circle is: {(2 * Math.PI * value):#.###}");
             Console.WriteLine($"The area of the circle is: {(Math.PI * value * value):#.###}");
--- Exercises/Week1GradedExercises/Triangle/Program.cs
+++ Exercises/Week1GradedExercises/Triangle/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Triangle
 {
@@ -11,9 +7,9 @@ namespace Triangle
         static void Main(string[] args)
         {
             Console.WriteLine("This application uses Herron's formula to find the area of a triangle.");
-            double a = getInput("a");
-            double b = getInput("b");
-            double c = getInput("c");
+            double a = GetInput("a");
+            double b = GetInput("b");
+            double c = GetInput("c");
             double p = (a + b + c) / 2;
             double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
             if (area >= 0.000000001)
@@ -21,13 +17,21 @@ namespace Triangle
             else
                 Console.WriteLine("The lengths given are impossible for a triangle.");
         }
-        private static double getInput(string s)
+        private static double GetInput(string s)
         {
             double value;
             do
             {
                 Console.Write($"Enter a the length of side {s}: ");
-                value = double.Parse(Console.ReadLine());
+                try
+                {
+                    value = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please use only numbers.");
+                    value = -1;
+                }
             } while (value <= 0);
             return value;
         }
