ee0c85a90375a98d12d7b352e37f71b07b65f065
jasonbergener <30800131+jasonbergener@users.noreply.github.com>
2017-09-11 15:37:23 -0400

    Add files via upload

---
+++ Exercises/Week5GradedExercise/Week5GradedExercise/Program.cs
@@ -0,0 +1,345 @@
+﻿using System;
+/* 
+ * Author: Jason Bergener
+ * Description: The instructions were to use arrays to return the winning bets
+ * that result in each possible outcome of the roulette wheel.
+*/
+
+namespace Week5GradedExercise
+{
+    class Program
+    {
+        private static Table table = new Table(); // made the table a field so all the methods could get to it.
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome to the roulette table!");
+                Roulette(); // Re-directed main method to allow catching all possible exceptions.
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void Roulette()
+        {
+            Random random = new Random(); //random number generator
+            while (true) //open the infinite loop for user input
+            {
+                try //try-catch to keep random characters from messing up the program
+                {
+                    Console.Write("Enter a number to check, N to stop, or S to spin: "); //prompt user
+                    string more = Console.ReadLine(); //user input
+                    if (more.ToLower() == "n") break; //if they want to quit
+                    else if (more.ToLower() == "s") GetWins(random.Next(38)); //if they want to spin
+                    else if (more == "00") GetWins(37); //if they enter 00 (which is 37 for our purposes)
+                    else //entered anything other than n, s, or 00
+                    {
+                        int value = int.Parse(more); //change the input to an int
+                        if (value >= 0 && value <= 36) GetWins(value); //if int is in range
+                        else Console.WriteLine("Enter a value between 0 and 36 or 00."); //if int is out of range
+                    }
+                }
+                catch (Exception) //catches non-int other than n, s, and 00
+                {
+                    Console.WriteLine("Enter a value between 0 and 36 or 00.");
+                }
+
+            }
+        }
+
+        static void GetWins(int i) //returns the winning bets for the number provided (37 for 00)
+        {
+            //The first line checks if the number is 37 and returns the appropriate response for 00.
+            if (i == 37) Console.Write($"The following bets win on 00:\nThe Straight Up win is 00.\n");
+            //The next two lines work for the rest of the numbers
+            else Console.Write($"The following bets win on {i}:\nThe Straight Up win is {i}.\n");
+            Console.Write($"The Split wins are: {Split(i)}.\n");
+
+            if (i > 0 && i < 37) //only non-zero numbers have these winning bets
+            {
+                Console.Write($"The Street win is: ({Street(i)}).\n" +
+                $"The Corner wins are: {Corner(i)}.\n" +
+                $"The Line wins are: {Line(i)}.\n" +
+                $"The Column win is: {Column(i)}.\n" +
+                $"The Dozen win is: {Dozen(i)}.\n" +
+                $"The Low/High number win is: {Low_High(i)}.\n" +
+                $"The Odd/Even win is: {Odd_Even(i)}.\n" +
+                $"The Color win is: {table.GetColor(i)}.\n");
+            }
+            if (i < 4 || i == 37) Console.Write($"The Basket bet wins (0, 00, 1, 2, 3).\n");//checks if the basket bet wins
+            Console.WriteLine();//adds an extra line to seperate multiple spins
+        }
+
+        static string Split(int i)//returns numbers adjacent to the input.
+        {
+            if (table.Column(i)==0)//if the number is in the first column
+            {
+                if (table.Row(i) == 0)//if the number is in the first row
+                {
+                    return $"({table.GetNum(table.Row(i), table.Column(i))}," + //the number
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))})," + //to the right
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," + //the number
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})"; //below
+                }
+                else if (table.Row(i) == 11)//if the number is in the last row
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), table.Column(i))}," + //above
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," + //the number
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," + //the number
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))})"; //to the right
+                }
+                else
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), table.Column(i))}," + //above
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," + //the number
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," + //the number
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))})," +//to the right
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," + //the number
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})"; //below
+                }
+            }
+            else if (table.Column(i) == 1) //if the number is in the second column
+            {
+                if (table.Row(i) == 0) //the notes below here were redundant and thus omitted
+                {
+                    return $"({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})";
+                }
+                else if (table.Row(i) == 11)
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))})";
+                }
+                else
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})";
+                }
+            }
+            else if (table.Column(i) == 2)
+            {
+                if (table.Row(i) == 0)
+                {
+                    return $"({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})";
+                }
+                else if (table.Row(i) == 11)
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})";
+                }
+                else
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})";
+                }
+            }
+            else //not in any of the columns
+            {
+                return "(0, 00)";
+            }
+        }
+
+        static string Street(int i) //returns the numbers in the same row as the input
+        {
+            return $"{table.GetNum(table.Row(i), 0)}," +
+                $" {table.GetNum(table.Row(i), 1)}," +
+                $" {table.GetNum(table.Row(i), 2)}";
+        }
+
+        static string Corner(int i) //returns each four number set that shares a corner with the input
+        {
+            if (table.Column(i) == 0) //same concept as Split with checking for column and row before returning results
+            {
+                if (table.Row(i) == 0)
+                {
+                    return $"({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), (table.Column(i) + 1))})";
+                }
+                else if (table.Row(i) == 11)
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) - 1), (table.Column(i) + 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))}),";
+                }
+                else
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) - 1), (table.Column(i) + 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), (table.Column(i) + 1))})";
+                }
+            }
+            else if (table.Column(i) == 1)
+            {
+                if (table.Row(i) == 0)
+                {
+                    return $"({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), (table.Column(i) - 1))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), (table.Column(i) + 1))})";
+                }
+                else if (table.Row(i) == 11)
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), (table.Column(i) - 1))}," +
+                        $" {table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) - 1), (table.Column(i) + 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))})";
+                }
+                else
+                {
+                    return $"" +
+                        $"({table.GetNum((table.Row(i) - 1), (table.Column(i) - 1))}," +
+                        $" {table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) - 1), (table.Column(i) + 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))})," +
+                        $" ({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), (table.Column(i) - 1))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) + 1))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), (table.Column(i) + 1))})" +
+                        $"";
+                }
+            }
+            else
+            {
+                if (table.Row(i) == 0)
+                {
+                    return $"({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), (table.Column(i) - 1))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})";
+                }
+                else if (table.Row(i) == 11)
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), (table.Column(i) - 1))}," +
+                        $" {table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})";
+
+                }
+                else
+                {
+                    return $"({table.GetNum((table.Row(i) - 1), (table.Column(i) - 1))}," +
+                        $" {table.GetNum((table.Row(i) - 1), table.Column(i))}," +
+                        $" {table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))})," +
+                        $" ({table.GetNum(table.Row(i), (table.Column(i) - 1))}," +
+                        $" {table.GetNum(table.Row(i), table.Column(i))}," +
+                        $" {table.GetNum((table.Row(i) + 1), (table.Column(i) - 1))}," +
+                        $" {table.GetNum((table.Row(i) + 1), table.Column(i))})";
+                }
+            }
+        }
+
+        static string Line(int i)
+        {
+            if (table.Row(i) == 0)
+            {
+                return $"({table.GetNum(table.Row(i), 0)}," +
+                $" {table.GetNum(table.Row(i), 1)}," +
+                $" {table.GetNum(table.Row(i), 2)}," +
+                $" {table.GetNum((table.Row(i) + 1), 0)}," +
+                $" {table.GetNum((table.Row(i) + 1), 1)}," +
+                $" {table.GetNum((table.Row(i) + 1), 2)})";
+            }
+            else if (table.Row(i) == 11)
+            {
+                return $"({table.GetNum((table.Row(i) - 1), 0)}," +
+                $" {table.GetNum((table.Row(i) - 1), 1)}," +
+                $" {table.GetNum((table.Row(i) - 1), 2)}," +
+                $" {table.GetNum(table.Row(i), 0)}," +
+                $" {table.GetNum(table.Row(i), 1)}," +
+                $" {table.GetNum(table.Row(i), 2)})";
+            }
+            else
+            {
+                return $"({table.GetNum((table.Row(i) - 1), 0)}," +
+                $" {table.GetNum((table.Row(i) - 1), 1)}," +
+                $" {table.GetNum((table.Row(i) - 1), 2)}," +
+                $" {table.GetNum(table.Row(i), 0)}," +
+                $" {table.GetNum(table.Row(i), 1)}," +
+                $" {table.GetNum(table.Row(i), 2)}), " +
+                $" ({table.GetNum(table.Row(i), 0)}," +
+                $" {table.GetNum(table.Row(i), 1)}," +
+                $" {table.GetNum(table.Row(i), 2)}," +
+                $" {table.GetNum((table.Row(i) + 1), 0)}," +
+                $" {table.GetNum((table.Row(i) + 1), 1)}," +
+                $" {table.GetNum((table.Row(i) + 1), 2)})";
+            }
+        }
+
+        static string Column(int i) //returns a result based on the column of the input
+        {
+            if (table.Column(i) == 0) return "1st (1, 4, ... 31, 34)";
+            else if (table.Column(i) == 1) return "2nd (2, 5, ... 32, 35)";
+            else return "3rd (3, 6, ... 33, 36)";
+        }
+
+        static string Dozen(int i) //returns which third of the numbers the input is in
+        {
+            if (i < 19 && i > 0) return "1st (1-12)";
+            else if (i < 19 && i > 0) return "2nd (13-24)";
+            else return "3rd (25-36)";
+        }
+
+        static string Low_High(int i) //returns which half of the numbers the input is in
+        {
+            if (i < 19 && i > 0) return "low (1-18)";
+            else return "high (19-36)";
+        }
+
+        static string Odd_Even(int i) //returns odd or even based on input
+        {
+            if (i % 2 == 0) return "even";
+            else return "odd";
+        }
+    }
+}
---
+++ Exercises/Week5GradedExercise/Week5GradedExercise/Table.cs
@@ -0,0 +1,123 @@
+﻿using System;
+/* 
+ * Author: Jason Bergener
+ * Description: The instructions were to use arrays to return the winning bets
+ * that result in each possible outcome of the roulette wheel.
+ * This class supports the main program and encapsulates the table data.
+*/
+
+namespace Week5GradedExercise
+{
+    class Table
+    {
+        private int[,] table = new int[12, 3] { //defines the structure of the table
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 },
+            { 10, 11, 12 },
+            { 13, 14, 15 },
+            { 16, 17, 18 },
+            { 19, 20, 21 },
+            { 22, 23, 24 },
+            { 25, 26, 27 },
+            { 28, 29, 30 },
+            { 31, 32, 33 },
+            { 34, 35, 36 }
+        };
+        //provides the default color scheme for a roulette wheel/table
+        private int[] black = new int[18] { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
+        private int[] red = new int[18] { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+
+        public int GetNum(int row, int column) //returns the value of the table element
+        {
+            return table[row, column];
+        }
+
+        public string GetColor(int i) //returns the color of the input (works with RandomColors())
+        {
+            string color = null;
+            if (i == 0 || i == 37)
+            {
+                color = "green";
+            }
+            foreach (int c in black)
+                if (c == i)
+                {
+                    color = "black";
+                }
+            foreach (int c in red)
+                if (c == i)
+                {
+                    color = "red";
+                }
+            return color;
+        }
+
+        public int Column(int i) //returns the column of the input
+        {
+            for (int row = 0; row < 12; row++) //steps through each row
+            {
+                for (int column = 0; column < 3; column++) //steps through each column of each row
+                {
+                    if (table[row, column] == i) //looks for the match
+                    {
+                        return column;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public int Row(int i) //returns the row of the input
+        {
+            for (int row = 0; row < 12; row++) //steps through each row
+            {
+                for (int column = 0; column < 3; column++) //steps through each column of each row
+                {
+                    if (table[row, column] == i) //looks for the match
+                    {
+                        return row;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public void RandomColors() //This is unimplemented, but allows randomizing the colors
+        {
+            int blackCount = 0; //these make sure there are an equal number of each color (18)
+            int redCount = 0;
+            Random random = new Random();
+            for (int i = 1; i < 37; i++)
+            {
+                int temp = (int)random.Next(2);
+                if (temp == 0)
+                {
+                    if (blackCount > 17)
+                    {
+                        red[redCount] = i;
+                        redCount++;
+                    }
+                    else
+                    {
+                        black[blackCount] = i;
+                        blackCount++;
+                    }
+                }
+                else if (temp == 1)
+                {
+                    if (redCount > 17)
+                    {
+                        black[blackCount] = i;
+                        blackCount++;
+                    }
+                    else
+                    {
+                        red[redCount] = i;
+                        redCount++;
+                    }
+                }
+            }
+        }
+    }
+}
