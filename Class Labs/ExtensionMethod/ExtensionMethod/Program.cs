4c0e5e8cb226c436e7384fc5b403849ccbe6c46b
jasonbergener <30800131+jasonbergener@users.noreply.github.com>
2017-09-18 15:22:49 -0400

    Add files via upload

---
+++ Class Labs/ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Extensions;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void doWork()
+        {
+            int x = 591;
+            for (int i = 2; i <= 10; i++)
+            {
+                Console.WriteLine($"{x} in base {i} is {x.ConvertToBase(i)}");
+
+            }
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ Class Labs/GarbageCollectionDemo/GarbageCollectionDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine($"120 / 0 = {calculator.Divide(120, 0)}");
+            Console.WriteLine("Program finishing");
+            /*
+            using (Calculator calculator = new Calculator())
+            {
+                Console.WriteLine($"120 / 15 = {calculator.Divide(120, 15)}");             
+            }
+
+            Console.WriteLine("Program finishing");*/
+        }
+    }
+}
