f260e0ba5f23644f5124b5b74bdb052f795b63b8
Jason Bergener <jasonbergener@hotmail.com>
2017-09-19 10:35:28 -0400

    Uploaded a class lab

---
+++ Class Labs/AutomaticProperties/AutomaticProperties/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomaticProperties
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Polygon square = new Polygon();
+            Polygon triangle = new Polygon { NumSides = 3 };
+            Polygon pentagon = new Polygon { SideLength = 15.5, NumSides = 5 };
+            Console.WriteLine($"Square: number of sides is {square.NumSides}, " +
+                $"length of each side is {square.SideLength}");
+
+            Console.WriteLine($"Triangle: number of sides is {triangle.NumSides}, " +
+                $"length of each side is {triangle.SideLength}");
+
+            Console.WriteLine($"Pentagon: number of sides is {pentagon.NumSides}, " +
+                $"length of each side is {pentagon.SideLength}");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
