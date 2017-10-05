f4e73938d02acd86a2d368bfeb9daae60c085fe4
Jason Bergener <jasonbergener@hotmail.com>
2017-10-05 09:11:30 -0400

    Updated

---
+++ Class Labs/Drawing Using Properties/Drawing/IDraw.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Drawing
+{
+    interface IDraw
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        void Draw(Canvas canvas);
+    }
+}
